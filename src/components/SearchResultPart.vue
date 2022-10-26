5c3d8b1943498a1e8270ff232af451f382ffbbae
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-26 20:35:13 +0200

    component: create a comp for the parts of the result view

---
+++ src/components/SearchResultPart.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  value: string
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <div class="flex-1 pr-5 after:right-0 after:h-3">
+    <div class="text-sm text-dark-gray uppercase mb-4">{{ props.title }}</div>
+    <div class="text-very-dark-gray text-lg">{{ props.value }}</div>
+  </div>
+</template>
+
+<style scoped></style>
