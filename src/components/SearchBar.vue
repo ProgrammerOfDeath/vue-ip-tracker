11190597594a73f4c58e149914e8ffdb0edfc0cf
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-26 20:34:43 +0200

    template: adjust search bar template

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,20 @@
-<template>
-    <div>
-        <p class="">Hello</p>
-    </div>
-</template>
 <script lang="ts" setup>
+import { ref } from 'vue'
+const searchInput = ref('')
 </script>
-<style lang="css">
-    
-</style>
+<template>
+  <div class="flex h-14 w-[550px] shadow-md">
+    <input
+      class="py-4 flex-1 px-3 rounded-l-xl text-dark-gray"
+      type="text"
+      placeholder="Search for any IP address or domain"
+      v-model="searchInput"
+    />
+    <button
+      class="w-14 shrink-0 bg-black text-white rounded-r-xl active:bg-very-dark-gray transition-all ease-in-out duration-150"
+    >
+      >
+    </button>
+  </div>
+</template>
+<style lang="css"></style>
