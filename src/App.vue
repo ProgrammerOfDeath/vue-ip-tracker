f96565d4858f5905fece03fe4313228eacd799f2
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-26 20:35:24 +0200

    implement search bar and result view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
-import SearchBar from "./components/SearchBar.vue";
+import SearchBar from './components/SearchBar.vue'
+import SearchResultVue from './components/SearchResult.vue'
 </script>
 
 <template>
   <header>
-    <h1 class=" text-3xl text-very-dark-gray">IP Tracker</h1>
+    <h1 class="text-3xl text-very-dark-gray">IP Tracker</h1>
   </header>
-  <main class=" min-h-screen bg-very-dark-gray text-dark-gray">
-    <SearchBar />
+  <main class="min-h-screen bg-very-dark-gray text-dark-gray">
+    <div class="mx-auto w-2/3 pt-3">
+      <div class="flex justify-center mb-5">
+        <SearchBar />
+      </div>
+      <SearchResultVue />
+    </div>
   </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
