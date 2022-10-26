78de5c224a2fc77b1c49796b7da585a3378e3d33
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-26 20:34:58 +0200

    component: create comp for search result view

---
+++ src/components/SearchResult.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SearchResultPart from './SearchResultPart.vue'
+</script>
+<template>
+  <div
+    class="rounded-xl bg-white shadow-lg w-[1000px] h-[160px] px-9 py-10 flex"
+  >
+    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
+    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
+    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
+    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
+    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
+  </div>
+</template>
+
+<style scoped></style>
