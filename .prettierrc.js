f00484718b8407a84d8c3d8b8683201f92715178
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-26 20:34:30 +0200

    formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    semi: false,
-    tabWidth: 2,
-    useTabs: false,
-    printWidth: 80,
-    endOfLine: 'auto',
-    singleQuote: true,
-    bracketSpacing: true,
-    arrowParens: 'always'
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: 'auto',
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
 }
