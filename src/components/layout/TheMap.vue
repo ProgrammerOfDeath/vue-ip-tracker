aa2c4d2bc94884a57431c76089198bc24ef44b04
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-11-01 21:39:58 +0100

    component: create component for the map

---
+++ src/components/layout/TheMap.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import Leaf from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+  const titleLayer = Leaf.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }
+  )
+  const map = Leaf.map('map', {
+    center: [54.368992, 12.726375],
+    zoom: 15,
+    layers: [titleLayer],
+  })
+})
+</script>
+<template>
+  <div id="map" class="w-full h-[800px]"></div>
+</template>
+
+<style scoped></style>
