52f11f3ef757f22a1a927979c014ac63fecabb19
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-11-01 21:40:16 +0100

    layout: integrate map and adjust page layout

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
 import SearchBar from './components/SearchBar.vue'
 import SearchResultVue from './components/SearchResult.vue'
 import TheHeader from '@/components/layout/TheHeader.vue'
+import TheMap from '@/components/layout/TheMap.vue'
 </script>
 
 <template>
   <TheHeader />
-  <main class="min-h-screen text-dark-gray pt-12">
-    <div class="mx-auto w-2/3 pt-3">
+  <main class="min-h-screen text-dark-gray">
+    <div class="mx-auto absolute z-[1000] top-14 left-0 right-0 w-2/3">
       <div class="flex justify-center mb-5">
         <SearchBar />
       </div>
       <SearchResultVue />
     </div>
+    <TheMap />
   </main>
 </template>
 
--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="absolute left-0 right-0 top-0 z-0 shadow-lg">
-    <img :src="patternBg" alt="Background Pattern" />
+  <div class="relative left-0 right-0 top-0 z-0 shadow-lg h-56">
+    <img class="w-full" :src="patternBg" alt="Background Pattern" />
   </div>
 </template>
 
