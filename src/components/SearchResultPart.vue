baff7a3a33e4d71716fb4903f0d74f2969d61b88
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-11-01 07:00:43 +0100

    styling: adjust after element of result part

--- src/components/SearchResultPart.vue
+++ src/components/SearchResultPart.vue
@@ -7,10 +7,25 @@ interface Props {
 const props = defineProps<Props>()
 </script>
 <template>
-  <div class="flex-1 pr-5 after:right-0 after:h-3">
+  <div class="result-part flex-1 relative after:bg-dark-gray">
     <div class="text-sm text-dark-gray uppercase mb-4">{{ props.title }}</div>
     <div class="text-very-dark-gray text-lg">{{ props.value }}</div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.result-part::after {
+  content: '';
+  display: block;
+  position: absolute;
+  width: 1px;
+  height: 80%;
+  top: 50%;
+  bottom: 50%;
+  transform: translate(-50%, -50%);
+  right: 0;
+}
+.result-part:last-of-type::after {
+  display: none;
+}
+</style>
