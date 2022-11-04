a5a0287b757582e6ce512b10ab7ff932db37f567
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-11-04 10:45:28 +0100

    template: use store data in template

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-const searchInput = ref('')
+import { useTrackingStore } from '@/stores/tracking'
+
+const store = useTrackingStore()
 </script>
 <template>
   <div class="flex h-14 w-[550px] shadow-md z-10">
@@ -8,10 +9,11 @@ const searchInput = ref('')
       class="py-4 flex-1 px-3 rounded-l-xl text-dark-gray"
       type="text"
       placeholder="Search for any IP address or domain"
-      v-model="searchInput"
+      v-model="store.$state.ipAddress"
     />
     <button
       class="w-14 shrink-0 bg-black text-white rounded-r-xl active:bg-very-dark-gray transition-all ease-in-out duration-150"
+      @click="store.getTrackingData"
     >
       >
     </button>
--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
 import SearchResultPart from './SearchResultPart.vue'
+import { useTrackingStore } from '@/stores/tracking'
+import { computed } from '@vue/reactivity'
+
+const store = useTrackingStore()
 </script>
 <template>
   <div
     class="rounded-xl bg-white shadow-lg w-full h-[160px] px-9 py-10 flex gap-8 z-10 relative"
   >
-    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
-    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
-    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
-    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
-    <SearchResultPart :title="'Ip address'" :value="'Some Value'" />
+    <SearchResultPart
+      :title="'Ip address'"
+      :value="store.$state.trackingData?.ip ?? ''"
+    />
+    <SearchResultPart
+      :title="'Location'"
+      :value="store.$state.trackingData?.location?.region ?? ''"
+    />
+    <SearchResultPart
+      :title="'Timezone'"
+      :value="'UTC ' + (store.$state.trackingData?.location?.timezone ?? '')"
+    />
+    <SearchResultPart
+      :title="'ISP'"
+      :value="store.$state.trackingData?.isp ?? ''"
+    />
   </div>
 </template>
 
